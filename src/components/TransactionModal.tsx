cb7d241493c42753847ee1e3cab6110f326b691b
Selaelo1 <selaelolanga10@gmail.com>
2024-11-12 10:16:52 +0200

    Update TransactionModal.tsx

--- src/components/TransactionModal.tsx
+++ src/components/TransactionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { X, AlertTriangle } from "lucide-react";
 import { Transaction } from "../types/accounts";
 import { FraudClaimModal } from "./FraudClaimModal";
